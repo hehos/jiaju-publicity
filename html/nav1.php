1e1f7479e983784840bf010274f367c383108ae9
hehui <304574254@qq.com>
2015-08-21 19:23:38 +0800

    无缝隙滚动测试demo添加

--- html/nav1.php
+++ html/nav1.php
@@ -10,20 +10,37 @@
 <?php include "header.php" ?>
 
 <div class="container">
-    <?php include "nav-menu.php" ?>
+    <div class="cont-nav">
+        <div class="item on">
+            <a href="nav1.php">
+                <img src="../img/nav-1-on.png" alt=""/>
+            </a>
+        </div>
+        <div class="item">
+            <a href="nav2.php">
+                <img src="../img/nav-2.png" alt=""/>
+            </a>
+        </div>
+        <div class="item">
+            <a href="nav3.php">
+                <img src="../img/nav-3.png" alt=""/>
+            </a>
+        </div>
+    </div>
+    <div class="head-form from1">
+        <div class="form-cont">
+            <p class="current-num">今日已有<em>888</em>业主申请，本月申请人数已达<em>87878</em></p>
+            <form action="">
+                <input type="text" placeholder="您的称呼"/>
+                <input type="text" placeholder="手机号码"/>
+                <input type="text" placeholder="小区名称"/>
+                <input type="text" placeholder="验房时间"/>
+                <button type="submit">免费申请</button>
+            </form>
 
-    <div class="head-form">
-        <p class="current-num">今日已有222<em>88888</em>业主申请，本月申请人数已达<em>87878</em></p>
-        <form action="">
-            <input type="text" placeholder="您的称呼"/>
-            <input type="text" placeholder="手机号码"/>
-            <input type="text" placeholder="小区名称"/>
-            <input type="text" placeholder="验房时间"/>
-            <button type="submit">免费申请</button>
-        </form>
-
-        <p>拨打热线，快速了解九正家居装修</p>
-        <p><i></i>400-6464-001</p>
+            <p class="call-hintTxt">拨打热线，快速了解九正家居装修</p>
+            <p class="call"><img src="../img/call.png" alt=""/>400-6464-001</p>
+        </div>
     </div>
 
     <img src="../img/1-pk.png" alt=""/>
@@ -36,14 +53,13 @@
                 </div>
                 <div class="media-body">
                     <h4 class="media-title"><a href="">成都禾木艺景装饰工程有限公司</a></h4>
+                    <div class="media-labels1">
+                        <span>报名人数: 100</span>
+                        <span>验房师: 王麻子</span>
+                    </div>
                     <p class="media-text">成华区陆扇门经营部成立于1997年，下设成都市六扇门精品实
                         木门厂成华区陆扇门经营部成华区陆扇门经营部，是一家专业从事实木门、
                         套装门开发、生产、销售为一体的...</p>
-                    <div class="media-labels1">
-                        <span>主营: 实木门; 实木套装门</span>
-                        <span>类型: 生产厂家</span>
-                        <span>地址: 四川成都市大丰工业园区</span>
-                    </div>
                 </div>
             </div>
             <div class="media-firmitem">
@@ -52,14 +68,13 @@
                 </div>
                 <div class="media-body">
                     <h4 class="media-title"><a href="">成都禾木艺景装饰工程有限公司</a></h4>
+                    <div class="media-labels1">
+                        <span>报名人数: 100</span>
+                        <span>验房师: 王麻子</span>
+                    </div>
                     <p class="media-text">成华区陆扇门经营部成立于1997年，下设成都市六扇门精品实
                         木门厂成华区陆扇门经营部成华区陆扇门经营部，是一家专业从事实木门、
                         套装门开发、生产、销售为一体的...</p>
-                    <div class="media-labels1">
-                        <span>主营: 实木门; 实木套装门</span>
-                        <span>类型: 生产厂家</span>
-                        <span>地址: 四川成都市大丰工业园区</span>
-                    </div>
                 </div>
             </div>
         </div>
@@ -75,28 +90,31 @@
                 </thead>
                 <tbody>
                     <tr>
-                        <td>谢先生</td>
-                        <td>181********</td>
-                        <td>市政大院</td>
-                        <td>1分钟前</td>
+                        <td>谢先生</td><td>181********</td><td>市政大院</td><td>1分钟前</td>
                     </tr>
                     <tr>
-                        <td>王先生</td>
-                        <td>182********</td>
-                        <td>山水蓉城</td>
-                        <td>2分钟前</td>
+                        <td>王先生</td><td>182********</td><td>山水蓉城</td><td>2分钟前</td>
                     </tr>
                     <tr>
-                        <td>谢先生</td>
-                        <td>181********</td>
-                        <td>市政大院</td>
-                        <td>1分钟前</td>
+                        <td>谢先生</td><td>181********</td><td>市政大院</td><td>1分钟前</td>
                     </tr>
                     <tr>
-                        <td>王先生</td>
-                        <td>182********</td>
-                        <td>山水蓉城</td>
-                        <td>2分钟前</td>
+                        <td>王先生</td><td>182********</td><td>山水蓉城</td><td>2分钟前</td>
+                    </tr>
+                    <tr>
+                        <td>王先生</td><td>182********</td><td>山水蓉城</td><td>2分钟前</td>
+                    </tr>
+                    <tr>
+                        <td>王先生</td><td>182********</td><td>山水蓉城</td><td>2分钟前</td>
+                    </tr>
+                    <tr>
+                        <td>王先生</td><td>182********</td><td>山水蓉城</td><td>2分钟前</td>
+                    </tr>
+                    <tr>
+                        <td>王先生</td><td>182********</td><td>山水蓉城</td><td>2分钟前</td>
+                    </tr>
+                    <tr>
+                        <td>王先生</td><td>182********</td><td>山水蓉城</td><td>2分钟前</td>
                     </tr>
                 </tbody>
             </table>
--- html/nav3.php
+++ html/nav3.php
@@ -9,27 +9,45 @@
 <?php include "header.php" ?>
 
 <div class="container">
-    <?php include "nav-menu.php" ?>
+    <div class="cont-nav">
+        <div class="item on">
+            <a href="nav1.php">
+                <img src="../img/nav-1.png" alt=""/>
+            </a>
+        </div>
+        <div class="item">
+            <a href="nav2.php">
+                <img src="../img/nav-2.png" alt=""/>
+            </a>
+        </div>
+        <div class="item">
+            <a href="nav3.php">
+                <img src="../img/nav-3-on.png" alt=""/>
+            </a>
+        </div>
+    </div>
+    <div class="head-form from3">
+        <div class="form-cont">
+            <p class="title">10秒登记，帮您省心装修全程无忧</p>
+            <p class="current-num">今日已有<em>888</em>业主申请，本月申请人数已达<em>87878</em></p>
+            <form action="">
+                <input type="text" placeholder="您的称呼"/>
+                <input type="text" placeholder="手机号码"/>
+                <select name="zxtype">
+                    <option value="新房装修" selected="">新房装修</option>
+                    <option value="旧房翻新">旧房翻新</option>
+                    <option value="办公室装修">办公室装修</option>
+                    <option value="店铺装修">店铺装修</option>
+                    <option value="餐厅装修">餐厅装修</option>
+                    <option value="酒店装修">酒店装修</option>
+                    <option value="其他类型">其他类型</option>
+                </select>
+                <button type="submit">免费申请</button>
+            </form>
 
-    <div class="head-form">
-        <p class="current-num">今日已有222<em>88888</em>业主申请，本月申请人数已达<em>87878</em></p>
-        <form action="">
-            <input type="text" placeholder="您的称呼"/>
-            <input type="text" placeholder="手机号码"/>
-            <select name="zxtype">
-                <option value="新房装修" selected="">新房装修</option>
-                <option value="旧房翻新">旧房翻新</option>
-                <option value="办公室装修">办公室装修</option>
-                <option value="店铺装修">店铺装修</option>
-                <option value="餐厅装修">餐厅装修</option>
-                <option value="酒店装修">酒店装修</option>
-                <option value="其他类型">其他类型</option>
-            </select>
-            <button type="submit">免费申请</button>
-        </form>
-
-        <p>拨打热线，快速了解九正家居装修</p>
-        <p><i></i>400-6464-001</p>
+            <p class="call-hintTxt">拨打热线，快速了解九正家居装修</p>
+            <p class="call"><img src="../img/call.png" alt=""/>400-6464-001</p>
+        </div>
     </div>
 
     <img src="../img/3-pk.png" alt=""/>
